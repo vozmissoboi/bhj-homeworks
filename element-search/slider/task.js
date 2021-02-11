9c1fb85463ab11b0bb9d7b9b68f3b885e6fd2b37
Nataliia Kashyna <kashina1282@gmail.com>
2021-02-11 17:38:31 +0200

    first commit slider

---
+++ element-search/slider/task.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// Задача 3. Слайдер изображений
+
+const sliders = document.querySelectorAll('.slider__item');
+const arrows = document.querySelectorAll('.slider__arrow');
+
+Array.from(arrows).forEach(arrow => {
+    const direction = arrow.classList.contains('slider__arrow_prev') ? -1 : 1;
+    arrow.onclick = () => {
+        changeSlide(direction);
+    }
+})
+
+changeSlide = (direction) => {
+    const activeClass = 'slider__item_active';
+    let activeIndex = Array.from(sliders).findIndex(slider => slider.classList.contains(activeClass));
+    sliders[activeIndex].classList.remove(activeClass);
+
+    activeIndex = activeIndex + direction;
+
+    if (activeIndex > sliders.length - 1) {
+        activeIndex = 0;
+    }
+
+    if (activeIndex < 0) {
+        activeIndex = sliders.length - 1;
+    }
+
+    sliders[activeIndex].classList.add(activeClass);
+}
